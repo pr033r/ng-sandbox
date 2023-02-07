b5b3581df2925528d222249bd52def2aa96365c4
Adam Lasák <lasak.ad@gmail.com>
2023-02-07 23:59:42 +0100

    dependency providers

--- src/app/dependency-injection/dependency-providers/logger.legacy.ts
+++ src/app/dependency-injection/dependency-providers/logger.legacy.ts
@@ -1,6 +1,10 @@
 import { ILogger } from './ILogger';
 
-// Some very old legacy logger which is event not class, but we have to use it
+// Some very old legacy logger which is event not class, but we have to use it.
+// It cannot be used for 'useClass' or 'useExisting', because Angular injector
+// simply doesn't know (didn't create an instance) about it - it doesn't have
+// any Angular @Injectable decorator.
+// So, for this case, we have to use 'useValue'
 export const LegacyLogger: ILogger = {
   prefix: 'root',
   log(message: string) {
