e3a5469b61a33a74b4a0a8dd894d1ef647a634e5
Adam Lasák <lasak.ad@gmail.com>
2023-03-03 01:32:37 +0100

    APP_INITIALIZER

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConfigService } from './dependency-injection/initializer/config.service';
+import { switchMap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'ng-sandbox-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-sandbox';
+  constructor(private http: HttpClient, private config: ConfigService) {
+  }
+
+  ngOnInit() {
+    console.log('AppComponent init.');
+    // .subscribe() for execute the operator - or use 'async' is even better
+    // this.config.api$.pipe(
+    //   switchMap(url => this.http.get(`${url}/test`))
+    // ).subscribe();
+  }
 }
