152a526f2bff15db09d911493d53f26c21d0f758
Adam Lasák <lasak.ad@gmail.com>
2023-03-08 21:54:42 +0100

    design patterns

---
+++ src/app/design-patterns/design-patterns.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-sandbox-design-patterns',
+  template: ` <p>design-patterns works!</p> `,
+  styles: [],
+})
+export class DesignPatternsComponent {}
---
+++ src/app/design-patterns/design-patterns.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DesignPatternsComponent } from './design-patterns.component';
+
+@NgModule({
+  declarations: [DesignPatternsComponent],
+  imports: [CommonModule],
+})
+export class DesignPatternsModule {}
